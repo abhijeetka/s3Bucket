490459b7ad2a9f85d1a2e5fad5b02eac608adc94
Abhijeet Kamble <akamble@calibo.com>
2022-10-10 16:47:42 +0530

    added fix for deprication

--- main.tf
+++ main.tf
@@ -33,8 +33,8 @@ terraform {
 }
 
 resource "aws_s3_bucket" "demos3" {
-  bucket = "${var.bucket_name}"
-  acl = "${var.acl_value}"
+  bucket = var.bucket_name
+  acl = var.acl_value
 }
 
 ###### Variables #######
